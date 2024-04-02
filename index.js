a12a735638afd5d034cafab07380d2433c3de28f
cbhan0102 <cbhan0102@gmail.com>
2024-04-02 19:35:07 +0900

    [Chore]

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { writeFileSync } from 'node:fs';
 import Parser from "rss-parser";
 
 /**
- * README.MD에 작성될 페이지 텍스트
- * @type {string}
+ * README.MD
  */
+ 
 let text = `# 
 
 ## 📕 Latest Blog Posts
@@ -23,14 +23,12 @@ const parser = new Parser({
     const feed = await parser.parseURL('https://kongnamul-ability.tistory.com/rss');
 
     // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
-    if (feed.items && feed.items.item && feed.items.item.length > 0) {
-        for (let i = 0; i < Math.min(feed.items.item.length, 5); i++) {
-            const { title, link } = feed.items.item[i];
-            console.log(`${i + 1}번째 게시물`);
-            console.log(`추가될 제목: ${title}`);
-            console.log(`추가될 링크: ${link}`);
-            text += `<a href=${link}>${title}</a></br>`;
-        }
+    for (let i = 0; i < 5; i++) {
+        const {title, link} = feed.items[i];
+        console.log(`${i + 1}번째 게시물`);
+        console.log(`추가될 제목: ${title}`);
+        console.log(`추가될 링크: ${link}`);
+        text += `<a href=${link}>${title}</a></br>`;
     }
 
     // README.md 파일 작성
@@ -40,4 +38,3 @@ const parser = new Parser({
 
     console.log('업데이트 완료')
 })();
-
