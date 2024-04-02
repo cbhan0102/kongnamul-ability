ab3e3ef90c23b3e4611381d8641d90c164328852
cbhan0102 <cbhan0102@gmail.com>
2024-04-02 19:12:23 +0900

    [Add]readmeUpdate.js

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -5,18 +5,7 @@ import Parser from "rss-parser";
  * README.MD에 작성될 페이지 텍스트
  * @type {string}
  */
-let text = `# Hi there 👋
-
-## 이런 환경에 익숙해요✍🏼
-
-## 언어
-
-<p>
-  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
-  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
-</p>
-
-## Contact me
+let text = `# 
 
 ## 📕 Latest Blog Posts
 
@@ -31,15 +20,17 @@ const parser = new Parser({
 (async () => {
 
     // 피드 목록
-    const feed = await parser.parseURL('https://systorage.tistory.com/rss');
+    const feed = await parser.parseURL('https://kongnamul-ability.tistory.com/rss');
 
     // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
-    for (let i = 0; i < 5; i++) {
-        const {title, link} = feed.items[i];
-        console.log(`${i + 1}번째 게시물`);
-        console.log(`추가될 제목: ${title}`);
-        console.log(`추가될 링크: ${link}`);
-        text += `<a href=${link}>${title}</a></br>`;
+    if (feed.items && feed.items.item && feed.items.item.length > 0) {
+        for (let i = 0; i < Math.min(feed.items.item.length, 5); i++) {
+            const { title, link } = feed.items.item[i];
+            console.log(`${i + 1}번째 게시물`);
+            console.log(`추가될 제목: ${title}`);
+            console.log(`추가될 링크: ${link}`);
+            text += `<a href=${link}>${title}</a></br>`;
+        }
     }
 
     // README.md 파일 작성
@@ -49,3 +40,4 @@ const parser = new Parser({
 
     console.log('업데이트 완료')
 })();
+
